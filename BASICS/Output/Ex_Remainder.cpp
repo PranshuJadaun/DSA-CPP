89416c876e38ed7a3a8a1402a857c6abb496217b
Pranshu <pranshupratap@icloud.com>
2024-06-11 01:20:14 +0530

    Added Basic Output

---
+++ BASICS/Output/Ex_Remainder.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float a,b;
+    a = 10;
+    b = 6;
+    int t;
+    t = a/b;
+    float r;
+    r = a - b*t;
+    cout<<r;
+}
---
+++ BASICS/Output/modulusoperator.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<-11%7;
+    cout<<endl;
+}
